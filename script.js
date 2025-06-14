817cd73126c0fca986da01194dbe555a94f9f9cb
Yamen Morcel <77121917+Yamen-M@users.noreply.github.com>
2025-06-14 14:36:16 +0300

    fixed layout and added side bar

--- script.js
+++ script.js
@@ -1,20 +1,66 @@
 document.body.style.display = 'flex';
-document.body.style.justifyContent = 'center';
+document.body.style.justifyContent = 'space-between';
 document.body.style.alignItems = 'center';
 document.body.style.height = '100vh';
+document.body.style.margin = '0';
+document.body.style.padding = '0';
+document.documentElement.style.margin = '0';
+document.documentElement.style.padding = '0';
 
 const wrapper = document.createElement("div");
+const btnContainer = document.createElement("div");
+const gridContainer = document.createElement("div");
 
-const CustomGrid = document.createElement("button");
-const resetGrid = document.createElement("button");
-const toggleColor = document.createElement("button");
-const eraser = document.createElement("button");
+const btnCustomGrid = document.createElement("button");
+const btnResetGrid = document.createElement("button");
+const btnToggleColor = document.createElement("button");
+const btnErase = document.createElement("button");
+const titleDiv = document.createElement('div');
 
 
 
-function CreateSideBar()
+
+titleDiv.textContent = 'Etch-a-Sketch - :D';
+titleDiv.style.fontFamily = "Arial, sans-serif"
+titleDiv.style.fontSize = '24px';
+titleDiv.style.fontWeight = 'bold';
+titleDiv.style.marginBottom = '20px';
+titleDiv.style.paddingLeft = '5px'; 
+
+wrapper.style.display = "flex";
+wrapper.style.height = "100vh";
+
+gridContainer.style.flex = '1';              
+gridContainer.style.display = 'flex';       
+gridContainer.style.justifyContent = 'center';
+gridContainer.style.alignItems = 'center';
+gridContainer.style.paddingLeft = "300px";
+
+btnContainer.style.width = "250px";
+btnContainer.style.backgroundColor = "#eee";
+btnContainer.style.display = "flex";
+btnContainer.style.flexDirection = "column"
+btnContainer.style.justifyContent = "flex-start"
+btnContainer.style.gap = "10px"
+btnContainer.style.padding = "20px";
+btnContainer.style.height = "100vh";
+
+function createSideBar()
 {
+    btnContainer.appendChild(titleDiv);
+
+    btnCustomGrid.style.marginTop = "auto";
+    btnResetGrid.style.marginTop = "10px";
+    btnToggleColor.style.marginTop = "10px";
+    btnErase.style.marginTop = "10px";
+    btnErase.style.marginBottom = "225px"
+
+    btnContainer.appendChild(btnCustomGrid);
+    btnContainer.appendChild(rbtnResetGrid);
+    btnContainer.appendChild(btnToggleColor);
+    btnContainer.appendChild(btnErase);
     
+    wrapper.appendChild(btnContainer)
 }
 
 function createGrid()
@@ -36,8 +82,10 @@ function createGrid()
         mainContainer.appendChild(pixel);
     }
     
-    wrapper.appendChild(mainContainer)
+    gridContainer.appendChild(mainContainer)
+    wrapper.appendChild(gridContainer)
     document.body.appendChild(wrapper)
 }
 
+createSideBar();
 createGrid();
